f25e7d39d63c24cbf3a234809ce7eb6a5dac9ccb
Jack Soslow <jsoslow2@gmail.com>
2020-01-02 16:53:11 -0700

    add doDataProcessingAndCreateChart Function

--- Skill Tracker/Controllers/HomePageViewController.swift
+++ Skill Tracker/Controllers/HomePageViewController.swift
@@ -46,52 +46,7 @@ class HomePageViewController: UIViewController {
             //Create Chart
             CurrentUserData.getLevelUps(skills: allSkills) { (levelUps) in
                 
-                let skillmanip = SkillManipulation.maxValueByDateBySkill(allLevelUps: CurrentUserData.allLevelUps)
-                
-                SkillManipulation.sumByDateBySkill(skillMax: skillmanip) { (allSkillCharts) in
-                    var i = 0
-                    for skill in CurrentUserData.skillsSorted!.reversed() {
-                        let values = allSkillCharts[skill]
-                        let lineChartDataSet = LineChartDataSet(values:  values, label: skill)
-                        lineChartDataSet.drawCirclesEnabled = false
-                        let color = Designs.colors[i]
-                       
-                        let gradientColors = [color.cgColor, color.withAlphaComponent(0.5).cgColor] as CFArray // Colors of the gradient
-                        let colorLocations:[CGFloat] = [1.0, 0.0] // Positioning of the gradient
-                        let colorSpace = CGColorSpaceCreateDeviceRGB()
-                        let gradient = CGGradient(colorsSpace: colorSpace, colors: gradientColors, locations: colorLocations) // Gradient Object
-                        lineChartDataSet.fill = Fill.fillWithLinearGradient(gradient!, angle: 90.0)
-                        
-                        if (CurrentUserData.skillsSorted!.count <= 2) {
-                            i += 6
-                        } else if (CurrentUserData.skillsSorted!.count <= 3) {
-                            i += 4
-                            } else if (CurrentUserData.skillsSorted!.count <= 4) {
-                            i += 3
-                        } else if  (CurrentUserData.skillsSorted!.count <= 6) {
-                            i += 2
-                        } else {
-                            i += 1
-                        }
-                        
-                        lineChartDataSet.setColor(NSUIColor(cgColor: color.cgColor))
-                        lineChartDataSet.fillAlpha = 1.0
-                        lineChartDataSet.drawFilledEnabled = true
-                        lineChartDataSet.drawValuesEnabled = false
-                        lineChartDataSet.mode = .horizontalBezier
-                        
-                        if skill == "Total Level" {
-                            lineChartDataSet.lineWidth = 7.0
-                            lineChartDataSet.setColor(NSUIColor(cgColor: Designs.darkBlack.cgColor))
-                            lineChartDataSet.drawValuesEnabled = true
-                        }
-                        self.lineChartData.addDataSet(lineChartDataSet)
-                    }
-                }
-                
-                LineChartCreator.createChart(lineChartView: self.lineChartView, data: self.lineChartData, miniDate: CurrentUserData.miniDate!)
-                
-                self.lineChartView.animate(yAxisDuration: 2.0)
+                LineChartCreator.doDataProcessingAndCreateChart(lineChartData: self.lineChartData, lineChartView: self.lineChartView)
             }
         }
         
@@ -121,19 +76,12 @@ class HomePageViewController: UIViewController {
                 
                 CurrentUserData.getLevelUps(skills: allSkills) { (levelUps) in
                     for skill in levelUps {
-                        let lineChartDataSet = LineChartDataSet(values: skill.value, label: skill.key)
-                        lineChartDataSet.drawCirclesEnabled = false
-                        lineChartDataSet.fillColor = NSUIColor(cgColor: Designs.colors.randomElement()!.cgColor)
-                        lineChartDataSet.drawFilledEnabled = true
-                        
-                        self.lineChartData.addDataSet(lineChartDataSet)
-                    }
-                    LineChartCreator.createChart(lineChartView: self.lineChartView, data: self.lineChartData, miniDate: CurrentUserData.miniDate!)
+                        LineChartCreator.doDataProcessingAndCreateChart(lineChartData: self.lineChartData, lineChartView: self.lineChartView)
                 }
             }
         }
     }
-    
+    }
 }
 
 extension HomePageViewController: UITableViewDataSource, skillCellDelegate {
--- Skill Tracker/Supporting Files/LineChartCreator.swift
+++ Skill Tracker/Supporting Files/LineChartCreator.swift
@@ -36,4 +36,53 @@ struct LineChartCreator {
         xAxis.valueFormatter = DateValueFormatter(miniTime : miniDate)
         
     }
+    
+    static func doDataProcessingAndCreateChart (lineChartData : LineChartData, lineChartView: LineChartView ) {
+        let skillmanip = SkillManipulation.maxValueByDateBySkill(allLevelUps: CurrentUserData.allLevelUps)
+        
+        SkillManipulation.sumByDateBySkill(skillMax: skillmanip) { (allSkillCharts) in
+            var i = 0
+            for skill in CurrentUserData.skillsSorted!.reversed() {
+                let values = allSkillCharts[skill]
+                let lineChartDataSet = LineChartDataSet(values:  values, label: skill)
+                lineChartDataSet.drawCirclesEnabled = false
+                let color = Designs.colors[i]
+               
+                let gradientColors = [color.cgColor, color.withAlphaComponent(0.5).cgColor] as CFArray // Colors of the gradient
+                let colorLocations:[CGFloat] = [1.0, 0.0] // Positioning of the gradient
+                let colorSpace = CGColorSpaceCreateDeviceRGB()
+                let gradient = CGGradient(colorsSpace: colorSpace, colors: gradientColors, locations: colorLocations) // Gradient Object
+                lineChartDataSet.fill = Fill.fillWithLinearGradient(gradient!, angle: 90.0)
+                
+                if (CurrentUserData.skillsSorted!.count <= 2) {
+                    i += 6
+                } else if (CurrentUserData.skillsSorted!.count <= 3) {
+                    i += 4
+                    } else if (CurrentUserData.skillsSorted!.count <= 4) {
+                    i += 3
+                } else if  (CurrentUserData.skillsSorted!.count <= 6) {
+                    i += 2
+                } else {
+                    i += 1
+                }
+                
+                lineChartDataSet.setColor(NSUIColor(cgColor: color.cgColor))
+                lineChartDataSet.fillAlpha = 1.0
+                lineChartDataSet.drawFilledEnabled = true
+                lineChartDataSet.drawValuesEnabled = false
+                lineChartDataSet.mode = .horizontalBezier
+                
+                if skill == "Total Level" {
+                    lineChartDataSet.lineWidth = 7.0
+                    lineChartDataSet.setColor(NSUIColor(cgColor: Designs.darkBlack.cgColor))
+                    lineChartDataSet.drawValuesEnabled = true
+                }
+                lineChartData.addDataSet(lineChartDataSet)
+            }
+        }
+        
+        LineChartCreator.createChart(lineChartView: lineChartView, data: lineChartData, miniDate: CurrentUserData.miniDate!)
+        
+        lineChartView.animate(yAxisDuration: 2.0)
+    }
 }
